fd40cdff9d7642ab39319ff9c317bba5b654c19c
Jack Britton <jackxbritton@gmail.com>
2020-11-23 22:43:55 -0500

    Swing!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use crossbeam::{bounded, Receiver, Sender};
 use serde::{Deserialize, Serialize};
+use std::cmp::{Ord, Ordering};
+use std::collections::VecDeque;
 use std::default::Default;
 use std::error::Error;
 use std::f64::consts::PI;
 use std::io::Read;
-use std::iter::once;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic;
 
-#[derive(Debug, Copy, Clone, PartialEq, Deserialize, Serialize)]
+// TODO(jack) What if the queue just keeps getting bigger?
+// TODO(jack) Track MIDI state to ignore redundant NOTE OFFs, handle redundant
+// NOTE ONs as NOTE OFF + NOTE ON.
+
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd, Deserialize, Serialize)]
 struct Note {
     time: f64,
     channel: u8,
@@ -16,48 +21,23 @@ struct Note {
     duration: f64,
 }
 
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
-struct MidiNote {
-    time: f64,
-    channel: u8,
-    note: u8,
-    velocity_and_duration: Option<(u8, f64)>,
-}
-
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
-struct Section {
-    notes: Vec<Note>,
-}
-
-impl Section {
-    fn render(&self) -> impl Iterator<Item = MidiNote> + '_ {
-        self.notes.iter().flat_map(
-            |&Note {
-                 time,
-                 channel,
-                 note,
-                 velocity,
-                 duration,
-             }| {
-                once(MidiNote {
-                    time,
-                    channel,
-                    note,
-                    velocity_and_duration: Some((velocity, duration)),
-                })
-                .chain(once(MidiNote {
-                    time: time + duration,
-                    channel,
-                    note,
-                    velocity_and_duration: None,
-                }))
-            },
-        )
+impl Eq for Note {}
+impl Ord for Note {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.time.partial_cmp(&other.time).unwrap()
     }
 }
 
 #[derive(Debug, Clone, Default, PartialEq, Deserialize, Serialize)]
+struct Section {
+    next_section: Option<usize>,
+    notes: Vec<Note>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Deserialize, Serialize)]
 struct Song {
+    rate: f64,
+    swing: f64,
     sections: Vec<Section>,
 }
 
@@ -66,15 +46,28 @@ impl Song {
     where
         R: Read,
     {
-        let song: Song = serde_yaml::from_reader(reader)?;
+        // Load the song and sort the notes.
+        let song = {
+            let mut song: Song = serde_yaml::from_reader(reader)?;
+            for section in &mut song.sections {
+                section.notes.sort_unstable();
+            }
+            song
+        };
+        // Validate.
         for section in &song.sections {
-            let mut notes: Vec<MidiNote> = section.render().collect();
-            for MidiNote {
+            if let Some(index) = section.next_section {
+                if !(0..song.sections.len()).contains(&index) {
+                    Err("next section index outside of valid range")?
+                }
+            }
+            for Note {
                 time,
                 channel,
                 note,
-                velocity_and_duration,
-            } in &notes
+                velocity,
+                duration,
+            } in &section.notes
             {
                 if !(0.0..1.0).contains(time) {
                     Err("section contains note with time not in the range 0.0..1.0")?;
@@ -85,46 +78,19 @@ impl Song {
                 if !(0..128).contains(note) {
                     Err("section contains note with note not in the range 0..128")?;
                 }
-                if let Some((velocity, duration)) = velocity_and_duration {
-                    if !(1..128).contains(velocity) {
-                        Err("section contains note with velocity not in the range 1..128")?;
-                    }
-                    if *duration <= 0.0 {
-                        Err("section contains note with non-positive duration")?;
-                    }
+                if !(1..128).contains(velocity) {
+                    Err("section contains note with velocity not in the range 1..128")?;
                 }
-            }
-            notes.sort_unstable_by(|MidiNote { time: t1, .. }, MidiNote { time: t2, .. }| {
-                t1.partial_cmp(t2).unwrap()
-            });
-            let mut midi_state = [[false; 128]; 16];
-            for MidiNote {
-                channel,
-                note,
-                velocity_and_duration,
-                ..
-            } in notes
-            {
-                let on = velocity_and_duration.is_some();
-                let slot = &mut midi_state[channel as usize][note as usize];
-                if *slot == on {
-                    Err("section sent redundant note event")?;
+                if *duration <= 0.0 {
+                    Err("section contains note with non-positive duration")?;
                 }
-                *slot = on;
-            }
-            if midi_state
-                .iter()
-                .flat_map(|notes| notes.iter())
-                .any(|&on| on)
-            {
-                Err("section left notes on")?;
             }
         }
         Ok(song)
     }
 }
 
-static RUNNING: AtomicBool = AtomicBool::new(true);
+static RUNNING: atomic::AtomicBool = atomic::AtomicBool::new(true);
 
 fn main() -> Result<(), Box<dyn Error>> {
     let (song_tx, song_rx): (Sender<Song>, Receiver<Song>) = bounded(0);
@@ -136,127 +102,126 @@ fn main() -> Result<(), Box<dyn Error>> {
     )?;
     let mut output_port = client.register_port("out", jack::MidiOut::default())?;
     let buffer_period = client.buffer_size() as f64 / client.sample_rate() as f64;
-    let mut rendered_events = Vec::with_capacity(64);
-    let mut midi_state = [[0; 128]; 16];
+    let mut song = Song::default();
     let mut clock = 0.0;
+    let mut clock_with_swing = 0.0;
+    let mut current_section = 0;
+    let mut event_queue: VecDeque<(f64, [u8; 3])> = VecDeque::with_capacity(32);
     let _active_client = client.activate_async(
         (),
         jack::ClosureProcessHandler::new(move |client, ps| {
-            let mut writer = output_port.writer(ps);
-
-            if !RUNNING.load(Ordering::Relaxed) {
-                let channels_and_notes =
-                    (0..16).flat_map(|channel| (0..128).map(move |note| (channel, note)));
-                for (channel, note) in channels_and_notes {
-                    if midi_state[channel][note] == 0 {
-                        continue;
-                    }
-                    let bytes = [0x80 | channel as u8, note as u8, 0];
-                    match writer.write(&jack::RawMidi {
-                        bytes: &bytes,
-                        time: 0,
-                    }) {
-                        Err(err) => {
-                            eprintln!("{}", err);
-                            return jack::Control::Continue;
-                        }
-                        _ => (),
-                    };
-                    midi_state[channel][note] = 0;
-                }
-                return jack::Control::Quit;
-            }
-
             if let Some(new_song) = song_rx.try_iter().last() {
-                // Re-render.
-                rendered_events.splice(
-                    ..,
-                    new_song
-                        .sections
-                        .get(0)
-                        .unwrap_or(&Section { notes: Vec::new() })
-                        .render(),
-                );
-                rendered_events.sort_unstable_by(
-                    |MidiNote { time: t1, .. }, MidiNote { time: t2, .. }| {
-                        t1.partial_cmp(t2).unwrap()
-                    },
-                );
-            }
-
-            let rate = 0.5;
-            let n = 4.0;
-            let max_swing = 1.0 - (n - 0.5) / n; // Btw.
-            let swing = 0.02_f64.min(0.9 * max_swing);
-            let dt = (rate * buffer_period) * (1.0 + swing * n * (2.0 * PI * n * clock).sin());
-            let new_clock = (clock + dt) % 1.0;
-
-            // TODO(jack) Swing.
-            // TODO(jack) Sections.
-
-            // According to the new clock, compute the desired MIDI state.
-            let new_midi_state = {
-                let mut midi_state = [[(0, 0.0); 128]; 16];
-                for &MidiNote {
-                    time,
-                    channel,
-                    note,
-                    velocity_and_duration,
-                } in rendered_events
-                    .iter()
-                    .take_while(|&&MidiNote { time, .. }| time < new_clock)
-                {
-                    let velocity = velocity_and_duration
-                        .map(|(velocity, _duration)| velocity)
-                        .unwrap_or(0);
-                    midi_state[channel as usize][note as usize] = (velocity, time);
-                }
-                midi_state
+                song = new_song;
             };
 
-            // Next, resolve the differences with the present MIDI state.
-            let channels_and_notes =
-                (0..16).flat_map(|channel| (0..128).map(move |note| (channel, note)));
-            for (channel, note) in channels_and_notes {
-                let (velocity, time) = new_midi_state[channel][note];
-                if midi_state[channel][note] == velocity {
-                    continue;
-                }
-                let bytes = [
-                    if velocity > 0 { 0x90 } else { 0x80 } | channel as u8,
-                    note as u8,
-                    velocity as u8,
-                ];
-                let sample = ((time - clock) / dt * client.buffer_size() as f64) as u32;
-                match writer.write(&jack::RawMidi {
-                    bytes: &bytes,
-                    time: sample,
-                }) {
-                    Err(err) => {
-                        eprintln!("{}", err);
-                        return jack::Control::Continue;
-                    }
-                    _ => (),
-                };
-            }
-            for (channel, notes) in midi_state.iter_mut().enumerate() {
-                for (note, velocity) in notes.iter_mut().enumerate() {
-                    *velocity = new_midi_state[channel][note].0;
+            let mut writer = output_port.writer(ps);
+
+            // If we're exiting, just drain the queue.
+            if !RUNNING.load(atomic::Ordering::Relaxed) {
+                while let Some((time, bytes)) = event_queue.pop_front() {
+                    let result = writer.write(&jack::RawMidi {
+                        bytes: &bytes,
+                        time: 0,
+                    });
+                    match result {
+                        Err(jack::Error::NotEnoughSpace) => {
+                            event_queue.push_front((time, bytes));
+                            return jack::Control::Continue;
+                        }
+                        Err(err) => eprintln!("{}", err),
+                        Ok(()) => (),
+                    };
                 }
             }
 
-            clock = new_clock;
+            // Advance the clock.
+            clock = clock + song.rate * buffer_period;
+            if clock >= 1.0 {
+                if song.sections.len() > 0 {
+                    current_section = (current_section + 1) % song.sections.len();
+                }
+                clock %= 1.0;
+            }
+
+            // Compute the clock with swing.
+            let old_clock_with_swing = clock_with_swing;
+            let n = 8.0;
+            let max_swing = 1.0 / (PI * n); // Maximum before the sinusoid beings bringing us backward in time.
+            clock_with_swing =
+                clock + song.swing * max_swing * (0.5 - 0.5 * (2.0 * PI * n * clock).cos()); // TODO(jack) What if the user changes the swing abruptly? Can we go back in time?
+
+            // Schedule notes.
+            for &Note {
+                time,
+                channel,
+                note,
+                velocity,
+                duration,
+            } in song
+                .sections
+                .get(current_section)
+                .unwrap_or(&Section::default())
+                .notes
+                .iter()
+                .filter(|&&Note { time, .. }| {
+                    if old_clock_with_swing <= clock_with_swing {
+                        old_clock_with_swing <= time && time < clock_with_swing
+                    } else {
+                        old_clock_with_swing <= time || time < clock_with_swing
+                    }
+                })
+            {
+                event_queue.push_back((
+                    (time - old_clock_with_swing + 1.0) % 1.0,
+                    [0x90 | channel, note, velocity],
+                ));
+                event_queue.push_back((
+                    (time + duration - old_clock_with_swing + 1.0) % 1.0,
+                    [0x80 | channel, note, 0],
+                ));
+            }
+
+            // Advance timers.
+            let dt = (clock_with_swing + 1.0 - old_clock_with_swing) % 1.0;
+            for (time, _) in &mut event_queue {
+                *time = *time - dt;
+            }
+
+            // Sort the event queue.
+            event_queue
+                .make_contiguous()
+                .sort_by(|(t1, _), (t2, _)| t1.partial_cmp(t2).unwrap());
+
+            // Emit note events.
+            while let Some((time, bytes)) = event_queue.pop_front() {
+                if time > 0.0 {
+                    event_queue.push_front((time, bytes));
+                    break;
+                }
+                let result = writer.write(&jack::RawMidi {
+                    bytes: &bytes,
+                    time: ((1.0 + time / dt) * client.buffer_size() as f64) as u32,
+                });
+                match result {
+                    Err(jack::Error::NotEnoughSpace) => {
+                        event_queue.push_front((time, bytes));
+                        return jack::Control::Continue;
+                    }
+                    Err(err) => eprintln!("{}", err),
+                    Ok(()) => (),
+                };
+            }
 
             jack::Control::Continue
         }),
     )?;
 
     ctrlc::set_handler(|| {
-        RUNNING.store(false, Ordering::Relaxed);
+        RUNNING.store(false, atomic::Ordering::Relaxed);
     })?;
 
     let mut song = Song::default();
-    while RUNNING.load(Ordering::Relaxed) {
+    while RUNNING.load(atomic::Ordering::Relaxed) {
         let new_song = {
             let file = std::fs::File::open("x.yaml")?;
             Song::from_reader(file)
